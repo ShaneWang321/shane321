78e3ee188b168b2972ea507f0762f60b2ebaf5b6
ShaneWang321 <111885799+ShaneWang321@users.noreply.github.com>
2023-04-10 01:05:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
-function copyLink() {
-  // 取得要複製的連結
-  var link = document.querySelector(".link").textContent;
-  
-  // 建立一個textarea元素
-  var textarea = document.createElement("textarea");
-  
-  // 將連結設定為textarea的值
-  textarea.value = link;
-  
-  // 將textarea加入到網頁中
-  document.body.appendChild(textarea);
-  
-  // 選取textarea中的內容
-  textarea.select();
-  
-  // 嘗試複製內容到剪貼簿
-  document.execCommand("copy");
-  
-  // 移除textarea元素
-  document.body.removeChild(textarea);
-  
-  // 提示複製成功
-  alert("Link copied to clipboard!");
+function copyToClipboard() {
+  const el = document.createElement('textarea');
+  el.value = 'https://shane321.vercel.app/'; // 在這裡替換成您要複製的網址
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+  alert('已複製網址到剪貼簿！');
 }
