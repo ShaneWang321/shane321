533398f7ffc4cb8a02de44e23f12adf92842bc61
ShaneWang321 <111885799+ShaneWang321@users.noreply.github.com>
2023-04-16 15:46:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-function copyToClipboard() {
-  const el = document.createElement('textarea');
-  el.value = 'https://shane321.vercel.app/'; // 在這裡替換成您要複製的網址
-  document.body.appendChild(el);
-  el.select();
-  document.execCommand('copy');
-  document.body.removeChild(el);
-  alert('已複製網址到剪貼簿！');
+function copyLink() {
+  var copyText = window.location.href;
+  navigator.clipboard.writeText(copyText);
+  alert("Link copied to clipboard!");
 }
